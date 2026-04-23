48b4999facfa483039de58f7f2dff7a1c0e220c4
Anthony <cosah@users.noreply.github.com>
2026-04-22 23:26:56 -0400

    just work damnit

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build',
+    outDir: 'dist',
   },  base: '/Winter2026-SI579FinalReact-Antshep/',
 })
